f6c3465dcfc9a6b8dad361a815a2275b31df05bf
Seth Viebrock <seth@viebrock.us>
2009-01-06 01:21:10 -0600

    Modified scout

--- scout/scratch/scout.rb
+++ scout/scratch/scout.rb
@@ -1,19 +1,30 @@
 require 'net/http'
+require 'uri'
 
 #http://www.dapper.net/RunDapp?dappName=Craigslist_XML_Minneapolis&v=1&v_item_name=guitar
-@base_url = 'http://www.dapper.net/RunDapp?dappName=Craigslist_XML_Minneapolis&v=1'
-@item_name = ARGV[0]
-puts "this is a network item query: " + @item_name
-@base_url = @base_url + "&v_item_name=" + @item_name
-puts "this is the full URL: " + @base_url
+base_url = 'http://www.dapper.net/RunDapp'
+item_name = ARGV[0]
 
-puts "this is the content from the full URL: "
+puts "this is the item we are querying: " + item_name
 
+#1: Simple POST. This will automatically construct the query string:
+response_object = Net::HTTP.post_form(URI.parse(base_url),
+                          {'dappName'=>'Craigslist_XML_Minneapolis', 'v'=>'1',
+                            'v_item_name' => item_name
+                          }
+                          )
 
+puts "Got HTTP status code " + response_object.code
 
-netHTTPobject = Net::HTTP.start(@base_url, 80 )
-responseObject = netHTTPobject.get('/')
-print responseObject.body
+#Check for HTTP status code above 200 and error if encountered:
+#First, convert the code to a number/integer via the "to_i" method and compare to 200:
+if response_object.code.to_i > 200
+  puts "Got bad response after attempting to query the following URL: #{base_url}"
+  exit 1
+end
+
+xml = response_object.body
+
 
 
 
