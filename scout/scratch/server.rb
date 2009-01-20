be000a9e1398dfdcb49b15393ccf5bf469b65bc9
sethviebrock <seth@viebrock.us>
2009-01-20 00:23:08 -0600

    Update server.rb

--- scout/scratch/server.rb
+++ scout/scratch/server.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'xmlsimple'
 require 'pp'
 
-#http://www.dapper.net/RunDapp?dappName=Craigslist_XML_Minneapolis&v=1&v_item_name=guitar
+#http://www.dapper.net/RunDapp?dappName=GuitarCenter&v=1&v_item_name=fender
 base_url = 'http://www.dapper.net/RunDapp'
 item_name = ARGV[0]
 
@@ -12,7 +12,7 @@ puts "this is the item we are querying: " + item_name
 
 #1: Simple POST. This will automatically construct the query string:
 response_object = Net::HTTP.post_form(URI.parse(base_url),
-                          {'dappName'=>'Craigslist_XML_Minneapolis', 'v'=>'1',
+                          {'dappName'=>'GuitarCenter', 'v'=>'1',
                             'v_item_name' => item_name
                           }
                           )
@@ -29,10 +29,27 @@ end
 xml = response_object.body
 
 doc = XmlSimple.xml_in xml
+
+#A datastructure is just a complex structure of variables, i.e. "a hash of arrays" or "an array of hashes"
+#These are sometimes called "multidimensional hashes" and "multidimensional arrays":
+print %{This is the printout of the 'doc' datastructure.
+'doc' is a hash with two keys that store arrays in them.
+We're only interested the 'listing' key, and we'll store its value (an array)
+in the variable called 'doc_listing_array' below
+\n
+}
+
+#Print out the doc datastructure:
 pp doc
 
+print %{This is the printout of the 'doc_listing_array' datastructure, derived from 
+print "the 'doc' datastructure:
+\n
+}
 
-
+doc_listing_array = doc["listing"]
+#Print out the doc_listing_array datastructure:
+pp doc_listing_array
 
 
 
